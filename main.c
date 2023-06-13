a709f48cdbe5622156a7a280a79a20270d5ce5cd
jerrysu5379 <48144074+jerrysu5379@users.noreply.github.com>
2023-06-13 01:28:27 +0000

    update

--- main.c
+++ main.c
@@ -1,5 +1,14 @@
-#include <stdio.h>
-
-int main(){
-    printf("hello");
+int CountDigit(int number, int digit) {
+    int count = 0;
+    if (number < 0) {
+    number = -number;
+}
+    if(number==0) return 1;
+    while (number != 0) {
+        if (number % 10 == digit) {
+            count++;
+        }
+        number /= 10;
+    }
+    return count;
 }
